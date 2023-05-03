afdd8acc5eb050491af133f1271b485e4b1e6283
Laxita-Gautam <131859801+Laxita-Gautam@users.noreply.github.com>
2023-05-03 21:30:20 +0530

    Create elseif.cpp

---
+++ Assignment 2/elseif.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int a;
+    cin>>a;
+    if(a>0){
+        cout<<"positive no.";
+    }
+    else if(a==0){
+        cout<<"zero";   
+    }
+    else
+    {
+        cout<<"negative no.";
+    }
+    return 0;
+}
