9d7541a475d521474142ecef50e1db41a73fd7b1
Laxita-Gautam <131859801+Laxita-Gautam@users.noreply.github.com>
2023-05-03 21:34:19 +0530

    Create nested.cpp

---
+++ Assignment 2/nested.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int a;
+    cin>>a;
+    if(a!=0){
+        if(a>0){
+        cout<<"positive no.and not zero";
+        }
+        else
+        {
+        cout<<"negative no. and not zero";
+        }
+    }
+    else{
+        cout<<"no. is zero";
+    }
+    return 0;
+}
