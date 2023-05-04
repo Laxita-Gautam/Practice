3d74f2d601644a528b5f47e80eebca3e225345c6
Laxita-Gautam <131859801+Laxita-Gautam@users.noreply.github.com>
2023-05-04 19:41:07 +0530

    Create pattern1.cpp

---
+++ Assignments 4/pattern1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int i,j,a;
+    for(i=1;i<=4;i++){
+        for(j=1;j<=4;j++){
+            cout<<"*";
+            }
+    cout<<"\n";
+    }
+    return 0;
+}
