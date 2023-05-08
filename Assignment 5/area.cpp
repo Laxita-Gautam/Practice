7d4d2335f3219379dfd37ffb69a59cbf6890f845
Laxita-Gautam <131859801+Laxita-Gautam@users.noreply.github.com>
2023-05-08 19:51:17 +0530

    Create area.cpp

---
+++ Assignment 5/area.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float a,r,c;
+    cout<<"Enter radius of circle";
+    cin>>r;
+    a=3.14*r*r;
+    c=2*3.14*r;
+    cout<<"area="<<a<<"\n"<<"circumfernce="<<c;
+    return 0;
+}
+
+
