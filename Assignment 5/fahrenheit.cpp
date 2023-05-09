d9d10cabf66c23af58f3258ac6332ef769529fd7
Laxita-Gautam <131859801+Laxita-Gautam@users.noreply.github.com>
2023-05-09 19:06:47 +0530

    Create fahrenheit.cpp

---
+++ Assignment 5/fahrenheit.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    float c,f;
+    cout<<"enter the temperature in centrigrade";
+    cin>>c;
+    f=(c*9/5)+32;
+    cout<<"in fahrenheit"<<f;
+    return 0;
+}
