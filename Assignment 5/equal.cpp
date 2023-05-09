9f4edef1fe66f1d8a7d52384f0ca201b78d0df71
Laxita-Gautam <131859801+Laxita-Gautam@users.noreply.github.com>
2023-05-09 19:15:24 +0530

    Create equal.cpp

---
+++ Assignment 5/equal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b;
+    cout<<"enter two no.";
+    cin>>a>>b;
+    if(a==b){
+        cout<<"Equal";   
+    }
+    else{
+        cout<<"not equal";
+    }
+    return 0;
+}
