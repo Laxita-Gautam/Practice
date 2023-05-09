97965cbef6d182c9cf4e8bb094a3255ccb8f90f6
Laxita-Gautam <131859801+Laxita-Gautam@users.noreply.github.com>
2023-05-09 20:55:14 +0530

    Create digitsum.cpp

---
+++ Assignment 5/digitsum.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,r;
+    int sum=0;
+    cin>>n;
+    while(n!=0){
+        r=n%10;
+        sum+=r;
+        n=n/10;
+    }
+    cout<<sum;
+    return 0;
+}
