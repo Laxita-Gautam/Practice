920bb4615a672c54360eb4aaab5a7d097308a655
Laxita-Gautam <131859801+Laxita-Gautam@users.noreply.github.com>
2023-05-09 19:11:26 +0530

    Create swap.cpp

---
+++ Assignment 5/swap.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a,b,c;
+    cout<<"enter two no. to swap";
+    cin>>a>>b;
+    c=a;
+    a=b;
+    b=c;
+    cout<<a<<"\n"<<b;
+    return 0;
+}
