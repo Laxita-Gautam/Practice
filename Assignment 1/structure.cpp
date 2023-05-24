5ee418e0c1fc1d3cc96966fd071045a7b8532c47
Laxita-Gautam <131859801+Laxita-Gautam@users.noreply.github.com>
2023-05-24 20:04:44 +0530

    Create structure.cpp

---
+++ Assignment 1/structure.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    struct New{
+        int age;
+        string first_name;
+        string last_name;
+        int standard;
+    }a;
+    cin>>a.age;
+    cin>>a.first_name;
+    cin>>a.last_name;
+    cin>>a.standard;
+    cout<<a.age<<" "<<a.first_name<<" "<<a.last_name<<" "<<a.standard;
+    return 0;
+}
