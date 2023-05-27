11c800111f13f881f2352788156165b620891cbf
Laxita-Gautam <131859801+Laxita-Gautam@users.noreply.github.com>
2023-05-27 20:42:33 +0530

    Create pattern6.cpp

---
+++ Assignments 4/pattern6.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int i,j;
+    for(i=1;i<=4;i++){
+        for(j=i;j>0;j--){
+            cout<<j;
+        }
+    cout<<"\n";
+    }
+    return 0;
+}
