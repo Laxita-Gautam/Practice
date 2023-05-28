0971289ad5eee49ed6f7f6feb7ffdd7ab51810c6
Laxita-Gautam <131859801+Laxita-Gautam@users.noreply.github.com>
2023-05-28 16:39:14 +0530

    Update Parallelogram pattern.cpp

--- Assignments 4/Parallelogram pattern.cpp
+++ Assignments 4/Parallelogram pattern.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+    int i,j,k;
+    for(i=1;i<=5;i++){
+        for(j=2;j<=i;j++){
+            cout<<" ";
+        }
+        for(k=1;k<=5;k++){
+            cout<<"*";
+        }
+    cout<<"\n";
+    }
+
+    return 0;
+}
 
