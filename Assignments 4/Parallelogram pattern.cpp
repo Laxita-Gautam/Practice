f568bdb932db7b1469698ce35b1f8ed9be167a13
Laxita-Gautam <131859801+Laxita-Gautam@users.noreply.github.com>
2023-05-30 20:01:54 +0530

    Update Parallelogram pattern.cpp

--- Assignments 4/Parallelogram pattern.cpp
+++ Assignments 4/Parallelogram pattern.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 int main() {
     int i,j,k;
-    for(i=1;i<=5;i++){
-        for(j=2;j<=i;j++){
+    for(i=0;i<5;i++){
+        for(j=1;j<=i;j++){
             cout<<" ";
         }
-        for(k=1;k<=5;k++){
+        for(k=0;k<5;k++){
             cout<<"*";
         }
     cout<<"\n";
